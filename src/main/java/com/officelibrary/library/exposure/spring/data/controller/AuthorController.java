97a919f605a47dc18844279b7faafda0f99f8470
y4oi <y4oi@nykredit.dk>
2022-08-13 14:23:05 +0200

    Added solution to exercise 1

---
+++ src/main/java/com/officelibrary/library/exposure/spring/data/controller/AuthorController.java
@@ -0,0 +1,39 @@
+package com.officelibrary.library.exposure.spring.data.controller;
+
+import java.util.List;
+
+import com.officelibrary.library.exposure.model.Author;
+import com.officelibrary.library.exposure.spring.data.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/libraryAPI")
+public class AuthorController {
+
+    private AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/authors/findByNameAndSurname")
+    public List<Author> findByNameAndSurname(@RequestParam(value = "name") String name, @RequestParam(value = "surname") String surname) {
+        return authorService.findByNameAndSurname(name, surname);
+    }
+
+    @GetMapping("/authors/findBySurnameNotLike")
+    public List<Author> findBySurnameNotLike(@RequestParam(value = "surname") String surname) {
+        return authorService.findBySurnameNotLike(surname);
+    }
+
+    @GetMapping("/authors/findByNamesLongerThan")
+    public List<Author> findBySurnameNotLike(@RequestParam(value = "minNameLength") Integer minNameLength) {
+        return authorService.findByNamesLongerThan(minNameLength);
+    }
+
+}
---
+++ src/main/java/com/officelibrary/library/exposure/spring/data/service/AuthorService.java
@@ -0,0 +1,30 @@
+package com.officelibrary.library.exposure.spring.data.service;
+
+import java.util.List;
+
+import com.officelibrary.library.exposure.model.Author;
+import com.officelibrary.library.exposure.spring.data.repository.AuthorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public List<Author> findByNameAndSurname(String name, String surname) {
+        return authorRepository.findByNameAndSurname(name, surname);
+    }
+
+    public List<Author> findBySurnameNotLike(String surname) {
+        return authorRepository.findBySurnameNotLike(surname);
+    }
+
+    public List<Author> findByNamesLongerThan(Integer minNameLength) {
+        return authorRepository.findByNamesLongerThan(minNameLength);
+    }
+
+}
