94d550191b3abe79f394921a80c4650c61b6e7ab
y4oi <y4oi@nykredit.dk>
2022-08-14 18:53:24 +0200

    Set up environment for exercise 2

--- src/main/java/com/officelibrary/library/exposure/spring/data/repository/AuthorRepository.java
+++ src/main/java/com/officelibrary/library/exposure/spring/data/repository/AuthorRepository.java
@@ -1,15 +1,5 @@
 package com.officelibrary.library.exposure.spring.data.repository;
 
-import java.util.List;
-
-import com.officelibrary.library.exposure.model.Author;
-
 public interface AuthorRepository {
-
-    public List<Author> findByNameAndSurname(String name, String surname);
-
-    public List<Author> findBySurnameNotLike(String surname);
-
-    public List<Author> findByNamesLongerThan(Integer minNameLength);
 
 }
---
+++ src/main/java/com/officelibrary/library/exposure/spring/data/service/AuthorService.java
@@ -0,0 +1,7 @@
+package com.officelibrary.library.exposure.spring.data.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+}
